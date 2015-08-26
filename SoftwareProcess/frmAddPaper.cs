4c7e3751c152f9384159aad90a9a4fcbc91666da
LeaderOfRogues <leader.of.rogues@gmail.com>
2015-08-27 11:06:42 +1200

    Fixing

--- SoftwareProcess/frmAddPaper.cs
+++ SoftwareProcess/frmAddPaper.cs
@@ -24,7 +24,7 @@ namespace SoftwareProcess
 
         private void btnPreAdd_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            
         }
 
         private void btnPreRemove_Click(object sender, EventArgs e)
