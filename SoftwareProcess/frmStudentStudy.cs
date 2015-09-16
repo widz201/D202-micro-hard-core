be9bf142c3f0538a73eec2b0a57efd97fc15da89
LeaderOfRogues <leader.of.rogues@gmail.com>
2015-09-17 09:45:06 +1200

    changing pretty pics

--- SoftwareProcess/frmStudentStudy.cs
+++ SoftwareProcess/frmStudentStudy.cs
@@ -50,23 +50,7 @@ namespace SoftwareProcess
                 MessageBox.Show("");
             }
         }
-            private void frmStudentStudy_Load(object sender, EventArgs e)
-            {
-                SqlConnection con = new SqlConnection(@"Data  Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\visits.mdf;Integrated  Security=True;User Instance=True");
-                    con.Open();
 
-                SqlCommand cmd = new SqlCommand("SELECT Paper_ID, Paper_Name", con);
-                SqlDataReader sdr = cmd.ExecuteReader();
-                while (sdr.Read())
-            {
-                tbxD111.Text = sdr["Paper_ID, Paper_Name"].ToString(); 
-                //this.txtCustomerAddress.Text = sdr["CustomerAddress"].ToString();
-                //this.txtArrivalTime.Text = sdr["ArrivalTime"].ToString();
-            }
-        
-            sdr.Close();
-
-            }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -113,13 +97,108 @@ namespace SoftwareProcess
 
         private void chkYear2_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (chkYear2.Checked == true)
+            {
+                tbxD101.Visible = false;
+                tbxD111.Visible = false;
+                tbxD101.Visible = false;
+                tbxD201.Visible = true;
+                tbxD202.Visible = true;
+                tbxD211.Visible = true;
+                tbxD301.Visible = false;
+                tbxD303.Visible = false;
+                tbxD311.Visible = false;
+                tbxI101.Visible = false;
+                tbxI121.Visible = false;
+                tbxI202.Visible = true;
+                tbxI203.Visible = true;
+                tbxI221.Visible = true;
+                tbxI102.Visible = false;
+                tbxI301.Visible = false;
+                tbxI302.Visible = false;
+                tbxI303.Visible = false;
+                tbxI321.Visible = false;
+                tbxT101.Visible = false;
+                tbxT111.Visible = false;
+                tbxT201.Visible = true;
+                tbxT205.Visible = true;
+                tbxT206.Visible = true;
+                tbxT211.Visible = true;
+                tbxT301.Visible = false;
+                tbxT302.Visible = false;
+                tbxT311.Visible = false;
+            }
 
         }
 
         private void chkWebDev_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (chkWebDev.Checked == true)
+            {
+                tbxD101.Visible = true;
+                tbxD111.Visible = true;
+                tbxD101.Visible = true;
+                tbxD201.Visible = false;
+                tbxD202.Visible = false;
+                tbxD211.Visible = true;
+                tbxD301.Visible = false;
+                tbxD303.Visible = true;
+                tbxD311.Visible = false;
+                tbxI101.Visible = true;
+                tbxI102.Visible = true;
+                tbxI111.Visible = true;
+                tbxI121.Visible = true;
+                tbxI202.Visible = true;
+                tbxI203.Visible = false;
+                tbxI221.Visible = true;
+                tbxI301.Visible = true;
+                tbxI302.Visible = true;
+                tbxI303.Visible = false;
+                tbxI321.Visible = false;
+                tbxT101.Visible = true;
+                tbxT111.Visible = true;
+                tbxT201.Visible = false;
+                tbxT205.Visible = false;
+                tbxT206.Visible = false;
+                tbxT211.Visible = false;
+                tbxT301.Visible = false;
+                tbxT302.Visible = false;
+                tbxT311.Visible = false;
+            }
+            else
+            {
+                tbxD101.Visible = true;
+                tbxD111.Visible = true;
+                tbxD101.Visible = true;
+                tbxD201.Visible = true;
+                tbxD202.Visible = true;
+                tbxD211.Visible = true;
+                tbxD301.Visible = true;
+                tbxD303.Visible = true;
+                tbxD311.Visible = true;
+                tbxI111.Visible = true;
+                tbxI101.Visible = true;
+                tbxI121.Visible = true;
+                tbxI202.Visible = true;
+                tbxI203.Visible = true;
+                tbxI221.Visible = true;
+                tbxI102.Visible = true;
+                tbxI301.Visible = true;
+                tbxI302.Visible = true;
+                tbxI303.Visible = true;
+                tbxI321.Visible = true;
+                tbxT101.Visible = true;
+                tbxT111.Visible = true;
+                tbxT201.Visible = true;
+                tbxT205.Visible = true;
+                tbxT206.Visible = true;
+                tbxT211.Visible = true;
+                tbxT301.Visible = true;
+                tbxT302.Visible = true;
+                tbxT311.Visible = true;
+            }
         }
+        
+        
     }
 }
