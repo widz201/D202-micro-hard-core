45879f9ff01435724f661807222888bf885922c6
LeaderOfRogues <leader.of.rogues@gmail.com>
2015-09-17 11:32:25 +1200

    coment

--- SoftwareProcess/frmStudentStudy.cs
+++ SoftwareProcess/frmStudentStudy.cs
@@ -140,8 +140,10 @@ namespace SoftwareProcess
                     string sName = myReader.GetString(1);
                     string sDescription = myReader.GetString(3);
                     int sCatergory = myReader.GetInt32(6);
+                    //string sPrereq = myReader.GetString(1);
                     lblPaperName.Text = sName;
                     lblPaperDesc.Text = sDescription;
+                    //lblPrereq.Text = sPrereq;
                     
                     switch (sCatergory)
                     {
