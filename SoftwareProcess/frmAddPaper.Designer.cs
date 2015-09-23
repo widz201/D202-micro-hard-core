edfb8dcb36a4a7ff6ad4009357c261082e22fa2a
William Ferguson <willyferguson@hotmail.co.nz>
2015-09-24 10:37:00 +1200

    alignment

--- SoftwareProcess/frmAddPaper.Designer.cs
+++ SoftwareProcess/frmAddPaper.Designer.cs
@@ -156,7 +156,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 75);
+            this.label2.Location = new System.Drawing.Point(6, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 6;
